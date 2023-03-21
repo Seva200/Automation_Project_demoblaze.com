917fac9c6d833b826310670bbecb4ebca02187c5
Seva200 <127044140+Seva200@users.noreply.github.com>
2023-03-21 12:25:54 +0200

    new

--- tests/TestHomePage.py
+++ tests/TestHomePage.py
@@ -2,10 +2,11 @@ import time
 
 from selenium.webdriver.common.by import By
 from src.utils.DriverSetUp import DriverSetUp
-
+import allure
 class TestHomePage(DriverSetUp):
 
 # Header
+
     def test_home_btn(self, driver):
         self.home_page.home_btn_click()
         assert driver.current_url == "https://www.demoblaze.com/index.html"
@@ -37,4 +38,41 @@ class TestHomePage(DriverSetUp):
         assert self.driver.find_element(By.CSS_SELECTOR, "#carouselExampleIndicators > ol > li.active")\
                    .get_attribute("class") == "active"
 
+    def test_left_arrow_btn(self, driver):
+        self.home_page.left_arrow_click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "#carouselExampleIndicators > ol > li:nth-child(3)") \
+                   .get_attribute("class") == "active"
+
+    def test_left_indicator(self, driver):
+        self.home_page.left_indicator_click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "#carouselExampleIndicators > ol > li:nth-child(1)") \
+                   .get_attribute("class") == "active"
+
+    def test_middle_indicator(self, driver):
+        self.home_page.middle_indicator_click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "#carouselExampleIndicators > ol > li.active") \
+                   .get_attribute("class") == "active"
+    def test_right_indicator(self, driver):
+        self.home_page.right_indicator_click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "#carouselExampleIndicators > ol > li:nth-child(3)") \
+                   .get_attribute("class") == "active"
+
+# Body
+    def test_categories_btn(self, driver):
+        self.home_page.categories_btn_click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "#cat").get_attribute("text") == "CATEGORIES"
+
+    def test_phones_btn(self, driver):
+        self.home_page.phones_btn_click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "#itemc").get_attribute("text") == "Phones"
+
+    def test_laptops_btn(self, driver):
+        self.home_page.laptops_btn_click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "#itemc").get_attribute("text") == "Laptops"
+    def test_monitors_btn(self, driver):
+        self.home_page.monitors_btn_click()
+        assert self.driver.find_element(By.CSS_SELECTOR, "#itemc").get_attribute("text") == "Monitors"
+    def test_item_btn(self, driver):
+        self.home_page.item_click()
+        assert self.driver.current_url == "https://www.demoblaze.com/prod.html?idp_=1"
     
